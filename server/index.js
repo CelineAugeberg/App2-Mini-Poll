d047f82529226fc7b7479dd27124eec99805068f
CelineAugeberg <celineaugeberg@gmail.com>
2026-01-25 17:35:00 +0100

    Fixed error
    Server starting error due to windows folder issue.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 
 const express = require("express");
-const validatePoll = require("./server/middleware/validatePoll.js");
-
+const validatePoll = require("./middleware/validatePoll");
 
 const app = express();
 const PORT = 3000;
